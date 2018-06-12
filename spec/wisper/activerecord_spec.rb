c817f67900be1ba4d9a61ccb7d27e6a5634012ee
Greg <gregpardo@gmail.com>
2018-06-12 11:35:20 -0400

    Updated tests for new callback broadcasts

--- spec/wisper/activerecord_spec.rb
+++ spec/wisper/activerecord_spec.rb
@@ -12,11 +12,13 @@ describe 'ActiveRecord' do
   describe 'validation' do
     it 'publishes an before_validation event to listener' do
       expect(listener).to receive(:before_validation).with(instance_of(model_class))
+      expect(listener).to receive(:before_meeting_validated).with(instance_of(model_class))
       model_class.subscribe(listener)
       model_class.create
     end
     it 'publishes an after_validation event to listener' do
       expect(listener).to receive(:after_validation).with(instance_of(model_class))
+      expect(listener).to receive(:after_meeting_validated).with(instance_of(model_class))
       model_class.subscribe(listener)
       model_class.create
     end
@@ -25,11 +27,13 @@ describe 'ActiveRecord' do
   describe 'create' do
     it 'publishes an before_create event to listener' do
       expect(listener).to receive(:before_create).with(instance_of(model_class))
+      expect(listener).to receive(:before_meeting_created).with(instance_of(model_class))
       model_class.subscribe(listener)
       model_class.create
     end
     it 'publishes an after_create event to listener' do
       expect(listener).to receive(:after_create).with(instance_of(model_class))
+      expect(listener).to receive(:after_meeting_created).with(instance_of(model_class))
       model_class.subscribe(listener)
       model_class.create
     end
@@ -42,12 +46,14 @@ describe 'ActiveRecord' do
 
     it 'publishes an before_update event to listener' do
       expect(listener).to receive(:before_update).with(instance_of(model_class))
+      expect(listener).to receive(:before_meeting_updated).with(instance_of(model_class))
       model.subscribe(listener)
       model.update(title: 'new title')
     end
 
     it 'publishes an after_update event to listener' do
       expect(listener).to receive(:after_update).with(instance_of(model_class))
+      expect(listener).to receive(:after_meeting_updated).with(instance_of(model_class))
       model.subscribe(listener)
       model.update(title: 'new title')
     end
@@ -60,35 +66,32 @@ describe 'ActiveRecord' do
 
     it 'publishes an before_destroy event to listener' do
       expect(listener).to receive(:before_destroy).with(instance_of(model_class))
+      expect(listener).to receive(:before_meeting_destroyed).with(instance_of(model_class))
       model_class.subscribe(listener)
       model.destroy
     end
 
     it 'publishes an after_destroy event to listener' do
       expect(listener).to receive(:after_destroy).with(instance_of(model_class))
+      expect(listener).to receive(:after_meeting_destroyed).with(instance_of(model_class))
       model_class.subscribe(listener)
       model.destroy
     end
   end
 
   describe 'commit' do
-    after do
-      model_class.subscribe(listener)
-      model_class.create
-    end
-
     it 'publishes an after_commit event to listener' do
       expect(listener).to receive(:after_commit).with(instance_of(model_class))
-    end
-
-    it 'publishes a <model_name>_committed event to listener' do
       expect(listener).to receive(:after_meeting_committed).with(instance_of(model_class))
+      model_class.subscribe(listener)
+      model_class.create
     end
   end
 
   describe 'rollback' do
     it 'publishes an after_rollback event to listener' do
       expect(listener).to receive(:after_rollback).with(instance_of(model_class))
+      expect(listener).to receive(:after_meeting_rolled_back).with(instance_of(model_class))
       model_class.subscribe(listener)
       model_class.transaction do
         model_class.create!
